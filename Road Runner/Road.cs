aa4dbf843b81d20ae8d932fdec96a2e0aee8208e
aichaachabtamayo-wq <aicha.achabtamayo@student.kdg.be>
2026-05-31 15:51:28 +0200

    added collectibles (spawning + points) added the effect of each collectible added increase of scroll speed after certain scores

--- Road Runner/Road.cs
+++ Road Runner/Road.cs
@@ -10,6 +10,10 @@ public class Road
     protected List<RoadElement> buffer; // buffer list used for the endless scrolling mechanic
     protected List<Vehicle> vehicles; // list that holds all vehicle objects on the road
     protected Random rndGen = new Random();
+    protected List<Collectible> collectibles;
+    protected double coinTimer = 0; // timer for coin spawning
+    protected double shieldTimer = 0; // timer for shield spawning
+    protected double slowmotionTimer = 0; // timer for slowmotion spawning
 
     public int Width
     {
@@ -35,6 +39,27 @@ public class Road
     {
         get { return rndGen; }
         set { rndGen = value; }
+    }
+    public List<Collectible> Collectibles
+    {
+        get { return collectibles; }
+        set { collectibles = value; }
+    }
+
+    public double CoinTimer
+    {
+        get { return coinTimer; }
+        set { coinTimer = value; }
+    }
+    public double ShieldTimer
+    {
+        get { return shieldTimer; }
+        set { shieldTimer = value; }
+    }
+    public double SlowmotionTimer
+    {
+        get { return slowmotionTimer; }
+        set { slowmotionTimer = value; }
     }
 
     public Road(int newWidth)
@@ -71,6 +96,7 @@ public class Road
         }
 
         vehicles = new List<Vehicle>();
+        collectibles = new List<Collectible>();
         SpawnVehicles();
     }
 
@@ -103,36 +129,6 @@ public class Road
             {
                 SpawnVehiclesOnRow(rowIndex);
             }
-        }
-    }
-
-    public void Scroll()
-    {
-        // remove the row just above the bottom border
-        RoadElement lastRow = rows[rows.Count - 2];
-        buffer.Add(lastRow);
-        rows.RemoveAt(rows.Count - 2);
-
-        int index = rndGen.Next(0, buffer.Count); // pick a random index between 0 and the number of rows in the buffer
-        rows.Insert(1, buffer[index]); // insert the randomly chosen row at position 1 (just after the top border)
-
-        foreach (Vehicle v in vehicles)
-        {
-            v.YPos++; // shift all vehicles down to match the scrolling map
-        }
-
-        // loop backwards so that when an element is removed, no elements are skipped
-        for (int i = vehicles.Count - 1; i >= 0; i--)
-        {
-            if (vehicles[i].YPos >= rows.Count - 1) // check if the vehicle has gone below the bottom border
-            {
-                vehicles.RemoveAt(i); // remove it from the list
-            }
-        }
-
-        if (rows[1].Type == RoadElementType.Road) // if the new top row is a road, spawn vehicles on it
-        {
-            SpawnVehiclesOnRow(1);
         }
     }
 
@@ -175,6 +171,94 @@ public class Road
             else
             {
                 Vehicles.Add(new Vehicle(VehicleType.SlowTruck, xPosition, rowIndex, ConsoleColor.DarkMagenta, "[===]", speed * 4, 0));
+            }
+        }
+    }
+
+    public void SpawnCollectibles(CollectibleType type, int playerYPos)
+    {
+        int x = rndGen.Next(1, width - 1); //random x pos
+        int y = rndGen.Next(1, playerYPos); // spawn above the player
+
+        switch (type)
+        {
+            case CollectibleType.Coin:
+                collectibles.Add(new Collectible(5, CollectibleType.Coin, x, y, "$", ConsoleColor.Yellow));
+                break;
+            case CollectibleType.Shield:
+                collectibles.Add(new Collectible(10, CollectibleType.Shield, x, y, "0", ConsoleColor.Cyan));
+                break;
+            case CollectibleType.Slowmotion:
+                collectibles.Add(new Collectible(15, CollectibleType.Slowmotion, x, y, "S", ConsoleColor.Magenta));
+                break;
+        }
+    }
+
+    public void UpdateCollectibles(double dt, int playerYPos)
+    {
+        coinTimer += dt;
+        shieldTimer += dt;
+        slowmotionTimer += dt;
+
+        if (coinTimer >= 5) // spawn coin every 5 seconds
+        {
+            SpawnCollectibles(CollectibleType.Coin, playerYPos);
+            coinTimer = 0;
+        }
+        if (shieldTimer >= 15) // spawn shield every 15 seconds
+        {
+            SpawnCollectibles(CollectibleType.Shield, playerYPos);
+            shieldTimer = 0; // reset timer
+        }
+        if (slowmotionTimer >= 20) // spawn slowmotion every 20 seconds
+        {
+            SpawnCollectibles(CollectibleType.Slowmotion, playerYPos);
+            slowmotionTimer = 0; // reset timer
+        }
+    }
+
+    public void Scroll()
+    {
+        // remove the row just above the bottom border
+        RoadElement lastRow = rows[rows.Count - 2];
+        buffer.Add(lastRow);
+        rows.RemoveAt(rows.Count - 2);
+
+        int index = rndGen.Next(0, buffer.Count); // pick a random index between 0 and the number of rows in the buffer
+        rows.Insert(1, buffer[index]); // insert the randomly chosen row at position 1 (just after the top border)
+
+        foreach (Vehicle v in vehicles)
+        {
+            v.YPos++; // shift all vehicles down to match the scrolling map
+        }
+
+        // loop backwards so that when an element is removed, no elements are skipped
+        for (int i = vehicles.Count - 1; i >= 0; i--)
+        {
+            if (vehicles[i].YPos >= rows.Count - 1) // check if the vehicle has gone below the bottom border
+            {
+                vehicles.RemoveAt(i); // remove it from the list
+            }
+        }
+
+        if (rows[1].Type == RoadElementType.Road) // if the new top row is a road, spawn vehicles on it
+        {
+            SpawnVehiclesOnRow(1);
+        }
+
+        //shift all collectibles down with the scroll
+        foreach(Collectible c in collectibles)
+        {
+            c.YPos++; // shift all collectibles down
+        }
+
+        // remove collectibles that have gone below the bottom
+        // loop backwards so that when an element is removed, no elements are skipped
+        for(int i = collectibles.Count - 1; i >= 0; i--)
+        {
+            if(collectibles[i].YPos >= rows.Count - 1) // check if collectible has gone below the bottom border
+            {
+                collectibles.RemoveAt(i); // remove it from the list
             }
         }
     }
